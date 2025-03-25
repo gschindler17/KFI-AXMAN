8e770c555fe5e44ded3cb751d4be6f6b510275ef
Garrett Schindler <67280331+gschindler17@users.noreply.github.com>
2025-03-25 15:22:34 -0500

    Configs

--- Main_KFI.py
+++ Main_KFI.py
@@ -5,13 +5,17 @@ from PythonFiles.KFI_Controller import KFI_Controller
 from PythonFiles.KFI_Logic import KFI_Logic
 
 if __name__ == "__main__":
-    app = QApplication(sys.argv)
+    try:
+        app = QApplication(sys.argv)
 
-    logic = KFI_Logic()  # Instantiate Model (Logic)
-    gui = KFI_GUI()  # Instantiate View (GUI)
-    controller = KFI_Controller(gui, logic)  # Pass GUI and Logic to Controller
+        logic = KFI_Logic()  # Instantiate Model (Logic)
+        gui = KFI_GUI()  # Instantiate View (GUI)
+        controller = KFI_Controller(gui, logic)  # Pass GUI and Logic to Controller
 
-    gui.set_controller(controller)  # Pass controller to GUI
-    gui.show()
-    
-    sys.exit(app.exec())
+        gui.set_controller(controller)  # Pass controller to GUI
+        gui.show()
+        
+        sys.exit(app.exec())
+    except KeyboardInterrupt:
+        print("\nKeyboardInterrupt detected. Exiting gracefully...")
+        sys.exit(0)
