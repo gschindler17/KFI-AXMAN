06a8c76da3280ecbedfdd4a458484d4b55db799c
Garrett Schindler <67280331+gschindler17@users.noreply.github.com>
2025-04-03 15:43:16 -0500

    Update KFI_Arduino.py

--- PythonFiles/KFI_Arduino.py
+++ PythonFiles/KFI_Arduino.py
@@ -49,8 +49,9 @@ class KFI_Arduino:
         with self.thread_lock:
             self.arduino.write(b'READ\n')  # 0xA0: Custom command to request a pin reading
             response = self.arduino.readline().decode().strip()
+            print("KFI_Arduino: Completed read all inputs:", response)
 
-        print("KFI_Arduino: Completed read all inputs:", response)  
+          
 
         return [
             True, True, True, True, 
