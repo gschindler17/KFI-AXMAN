f8eade3d47fb8aa603b765718d3ab9c2164b7571
Garrett Schindler <67280331+gschindler17@users.noreply.github.com>
2025-04-08 14:08:30 -0500

    readded thread locks

--- PythonFiles/KFI_Arduino.py
+++ PythonFiles/KFI_Arduino.py
@@ -12,7 +12,7 @@ class KFI_Arduino:
         self.input_pin_nums = [53,52,51,50,49,48,47,46,45,44,43,42,41]
         self.arduino = serial.Serial(comm_type, port_num, timeout=1)
         
-        # self.thread_lock = threading.Lock() #Prevents simultaneous writes
+        self.thread_lock = threading.Lock() #Prevents simultaneous writes
         
         
         time.sleep(0.05)
@@ -20,43 +20,42 @@ class KFI_Arduino:
 
     #Function to toggle pin and update label
     def toggle_output_pin(self, pin, bool_state):
-        if self.pin_nums[pin] not in self.pin_nums or bool_state not in [True, False]:
-            print("Invalid pin or state")
-            print("\nPin: ", pin, " State:", bool_state, "\n")
-            return
-        
-        state_set = "LOW"
-        if bool_state:
-            state_set = "HIGH"
+        with self.thread_lock:
+            if self.pin_nums[pin] not in self.pin_nums or bool_state not in [True, False]:
+                print("Invalid pin or state")
+                print("\nPin: ", pin, " State:", bool_state, "\n")
+                return
+            
+            state_set = "LOW"
+            if bool_state:
+                state_set = "HIGH"
 
-        message = f"{self.pin_nums[pin]},{state_set}\n"
-        print(f"Sent: {message.strip()}")
-        response = None
+            message = f"{self.pin_nums[pin]},{state_set}\n"
+            print(f"Sent: {message.strip()}")
+            response = None
 
-        # with self.thread_lock:
-        self.arduino.write(message.encode())  # Send data to Arduino
-        response = self.arduino.readline().decode().strip()
-        
-        # Read response from Arduino
-        if response:
-            print(f"Arduino Response: {response}")
+            
+            self.arduino.write(message.encode())  # Send data to Arduino
+            response = self.arduino.readline().decode().strip()
+            
+            # Read response from Arduino
+            if response:
+                print(f"Arduino Response: {response}")
         
     
     # SHOULD RETURN AN ARRAY OF BOOLEANS
     def READ_ALL_INPUTS(self):
         
-        # with self.thread_lock:
-        self.arduino.write(b'READ\n')  # 0xA0: Custom command to request a pin reading
-        response = self.arduino.readline().decode().strip()
-        print("KFI_Arduino: Completed read all inputs:", response)
+        with self.thread_lock:
+            self.arduino.write(b'READ\n')  # 0xA0: Custom command to request a pin reading
+            response = self.arduino.readline().decode().strip()
+            print("KFI_Arduino: Completed read all inputs:", response)
 
-          
-
-        return [
-            True, True, True, True, 
-            True, True, True, True, 
-            True, True, True, True
-            ] 
+            return [
+                True, True, True, True, 
+                True, True, True, True, 
+                True, True, True, True
+                ] 
 
 
     
