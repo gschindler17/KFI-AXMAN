a912f277b4789e6fcf72b716fa6405adf67da124
josephmoop <119136778+josephmoop@users.noreply.github.com>
2025-05-08 14:31:05 -0500

    popup

--- PythonFiles/KFI_GUI.py
+++ PythonFiles/KFI_GUI.py
@@ -100,6 +100,7 @@ class KFI_GUI(QMainWindow):
             self.controller.submit_bool_logic(self.boolInput.toPlainText())
         except RuntimeError as e:
             print(f"\n\nKFI_GUI: Exception[ERROR] Exception in submit_bool_logic: {e}\n\n")
+            QMessageBox.information('Error', 'Invalid Input')
         open_vals = [self.openRelay1.text(),self.openRelay2.text(), self.openRelay3.text()]
         close_vals =  [self.closeRelay1.text(),self.closeRelay2.text(), self.closeRelay3.text()]
         status_vals = [self.statusRelay1.text(), self.statusRelay2.text(), self.statusRelay3.text()]
